8111328110d8ac7890897401340a4abf4862f844
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-16 10:38:23 +0900

    演算・比較演算の確認

--- D48Q17.java
+++ D48Q17.java
@@ -2,6 +2,7 @@ public class D48Q17 {
     public static void main(String[] args) {
         int n = 35;
         System.out.println(n > 15 && n <= 30);
+        System.out.println(n > 15 || n <= 30);
     }
 }
 /*
