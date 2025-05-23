0381ef11677ddd0912904d587995e3e6f6ab2fd3
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-23 10:21:03 +0900

    commit

---
+++ D49Q17.java
@@ -0,0 +1,8 @@
+public class D49Q17 {
+    public static void main(String[] args) {
+        int n = 1;
+        System.out.println(++n + " " + n--);
+        //最終的な n の中身
+        System.out.println(n);
+    }
+}
---
+++ D49Q17kadai.java
@@ -0,0 +1,7 @@
+public class D49Q17kadai {
+    public static void main(String[] args) {
+        //D49Q17 の出力結果が 2 1 にと表示されるようにアレンジしてください
+        int n = 1;
+        System.out.println(++n + " " + --n);
+    }
+}
