f19cfad97e6d068ee88f10bcc6934db43a21e83b
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-23 11:27:13 +0900

    ２次元配列

---
+++ D49Q20kadai.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class D49Q20kadai {
+    //２次元配列
+    public static void main(String[] args) {
+        //double型の２次元配列 arr を 5行 10列 で作成してください
+        double[][] arr = new double[5][10];
+
+        //表示
+        System.out.println(Arrays.asList(arr));
+    }
+}
