3a3b41065bf127795c71f309a85c30a2e5160c87
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-23 10:30:24 +0900

    インクリメント演算子

--- D49Q17.java
+++ D49Q17.java
@@ -3,6 +3,7 @@ public class D49Q17 {
     public static void main(String[] args) {
         int n = 1;
         System.out.println(++n + " " + n--);
+
         //最終的な n の中身
         System.out.println(n);
     }
