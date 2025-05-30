e3415ed385ecb8e724d62b760736be6209eb88ee
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-30 11:57:53 +0900

    オブジェクトの振る舞い

--- D50Q23kadai.java
+++ D50Q23kadai.java
@@ -6,7 +6,6 @@ public class D50Q23kadai {
         D50Q23kadai obj = new D50Q23kadai();
         obj.setCode("Code");
 
-
         //System.out.println(code);             //NO
         //System.out.println(D50Q23kadai.code); //NO
         System.out.println(obj.code);           //OK
