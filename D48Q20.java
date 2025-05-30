53ba407c0accbfb9f62f447de185257b5b19fafc
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-30 10:15:08 +0900

    キャスト

---
+++ D48Q20.java
@@ -0,0 +1,10 @@
+public class D48Q20 {
+    //char の計算
+    public static void main(String[] args) {
+        //0x30 = 0
+        char c = 0x30;
+        c += 9;
+
+        System.out.println(c);
+    }
+}
