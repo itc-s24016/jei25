8dcaacb53dfab203bc7777e499724fd9223750b5
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-30 10:16:06 +0900

    int と double の計算

--- D48Q18.java
+++ D48Q18.java
@@ -3,7 +3,6 @@ public class D48Q18 {
     public static void main(String[] args) {
         double baseValue = 3.5;
         int units = 3;
-
         double width = baseValue * units;
 
         System.out.println(width);
