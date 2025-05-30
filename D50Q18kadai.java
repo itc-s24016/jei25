a49b05433c6a9982bdad8cbeb614ffc452f7ab3a
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-30 11:21:36 +0900

    i++と++iの違い

---
+++ D50Q18kadai.java
@@ -0,0 +1,9 @@
+public class D50Q18kadai {
+    //i++と++iの違い インクリメント
+    public static void main(String[] args) {
+        int num = 0;
+        while(++num < 11){
+            System.out.print(num);
+        }
+    }
+}
