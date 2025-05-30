ef9e04f77a1bb92aeb48259aac8e6ad1c2506a6f
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-30 10:23:28 +0900

    関数の引数と入力値の型

---
+++ D49Q26.java
@@ -0,0 +1,10 @@
+public class D49Q26 {
+    //関数の引数と入力値の型
+    public static void main(String[] args) {
+        D49Q26 obj = new D49Q26();
+        obj.show(5.5);
+    }
+    void show(double n){
+        System.out.println("Value is " + n);
+    }
+}
