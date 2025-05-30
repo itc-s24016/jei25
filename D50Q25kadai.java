f91522a3b89a05a59fe1974fc4d3892ac61d3a9b
itc-s24016 <s24016@std.it-college.ac.jp>
2025-05-30 12:05:44 +0900

    偶数か奇数か

---
+++ D50Q25kadai.java
@@ -0,0 +1,10 @@
+public class D50Q25kadai {
+    //偶数か奇数か
+    public static void main(String[] args) {
+        for (int i = 0; i <= 8; i++){
+            if (i % 2 == 1)   //0 にすれば偶数を表示できる
+                continue;
+            System.out.println(i);
+        }
+    }
+}
