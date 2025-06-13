d0d7823c7b4e243454b1a6f76168ce220335338d
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-13 11:43:16 +0900

    インクリメント演算子

--- D49Q17kadai.java
+++ D49Q17kadai.java
@@ -1,7 +1,7 @@
 public class D49Q17kadai {
     //インクリメント演算子
     public static void main(String[] args) {
-        //D49Q17 の出力結果が 2 1 にと表示されるようにアレンジしてください
+        //D49.D49Q17 の出力結果が 2 1 にと表示されるようにアレンジしてください
         int n = 1;
         System.out.println(++n + " " + --n);
     }
