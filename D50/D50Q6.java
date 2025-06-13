f3d8f1fd803e0f4af88a9c5ac575f2b06a8aee04
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-13 12:40:41 +0900

    入力値の範囲の素数を表示

--- D50/D50Q6.java
+++ D50/D50Q6.java
@@ -25,13 +25,14 @@ class PrimeNumbers{
     boolean[] isPrimeNumbers;
 
     void initializePrimeNumbers(int max){
+        //==== 素数？ はい/いいえ の配列を作成=====
+        //現在中身は [true true true ...] の状態
         isPrimeNumbers = new boolean[max + 1];
         for (int i = 2; i <= max; i++){
             isPrimeNumbers[i] = true;
-        }
-
-        //max = 16 なら p は 2 ..< 4
-        //max = 25 なら p は 2 ..< 5 まで続ける
+        }//====================================
+        //==== ここで true/false に分けていく ============
+        /* max = 25 なら p は 2 ..< 5 まで続ける */
         for (int p = 2; p <= Math.sqrt(max); p++){
             if (isPrimeNumbers[p]){
                 for (int m = p * 2; m <= max; m += p){
@@ -39,17 +40,18 @@ class PrimeNumbers{
                 }
             }
         }
-    }
+    }//================================================
 
     int[] getPart(int min, int max){
+        //==== 素数が何個あったか数える =======
         int count = 0;
         for (int n = min; n <= max; n++){
             if (isPrimeNumbers[n]){
                 count++;
             }
-        }
-
-        int [] part = new int[count];
+        }//===============================
+        //==== 素数を配列で返す =============
+        int [] part = new int[count]; //素数専用配列
         int index = 0;;
         for (int n = min; n <= max; n++){
             if (isPrimeNumbers[n]){
@@ -57,5 +59,5 @@ class PrimeNumbers{
             }
         }
         return part;
-    }
+    }//====================================
 }
