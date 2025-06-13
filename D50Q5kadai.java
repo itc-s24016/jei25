8c42d8258468c23d9278ecf2fdb92ee3c9b061b3
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-13 11:16:27 +0900

    配列のソート過程を表示

--- D50Q5kadai.java
+++ D50Q5kadai.java
@@ -7,17 +7,14 @@ public class D50Q5kadai {
             num[i] = Integer.parseInt(args[i]);
         }
 
-        int tmp;
         for (int i = 1; i < num.length; i++){
             for (int j = i -1; j >= 0 && num[j] > num[j + 1]; j--){
-                tmp = num[j];
+                int tmp = num[j];
                 num[j] = num[j + 1];
                 num[j + 1] = tmp;
-
                 display(num);
             }
-        }
-        System.out.println("--------------------");
+        }System.out.println("--------------------");
         display(num);
     }
 
@@ -25,7 +22,6 @@ public class D50Q5kadai {
         System.out.print("ソート途中:");
         for (int val : num){
             System.out.print(val + " ");
-        }
-        System.out.println();
+        }System.out.println();
     }
 }
