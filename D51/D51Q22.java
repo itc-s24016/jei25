9c44111b24dd3adb5ea3276d6de7424cce6411f6
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-27 12:18:37 +0900

    引数の型と入力値

---
+++ D51/D51Q22.java
@@ -0,0 +1,10 @@
+package D51;
+//引数の型と入力値
+public class D51Q22 {
+    public static void main(String[] args) {
+        view(false, "true");
+    }
+    public static void view(boolean b, String s) {
+        System.out.println(" " + b + s);
+    }
+}
