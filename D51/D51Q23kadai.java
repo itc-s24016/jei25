df40168e57d2d913d963a5af0ec9ab388392b6f6
itc-s24016 <s24016@std.it-college.ac.jp>
2025-06-27 12:28:54 +0900

    拡張 for 文を理解する

---
+++ D51/D51Q23kadai.java
@@ -0,0 +1,11 @@
+package D51;
+//拡張 for 文を理解する
+public class D51Q23kadai {
+    public static void main(String[] args) {
+        String[] season = {"春", "夏", "秋", "冬"};
+
+        for (String s : season) {
+            System.out.print(s + "。");
+        }
+    }
+}
