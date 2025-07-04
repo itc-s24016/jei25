f159bfe6225c2852edcfa505c9c5ae1b2906b62c
itc-s24016 <s24016@std.it-college.ac.jp>
2025-07-04 11:32:10 +0900

    引数を切り上げて表示

--- D51/D51kadai.java
+++ D51/D51kadai.java
@@ -1,4 +1,14 @@
 package D51;
 
+import static java.lang.Math.*;
 public class D51kadai {
+    /*
+    与えられた引数の2番目を実数に変換、切り上げて表示する
+    Math.cell()                  = 切り上げ
+    Double.parseDouble(String s) = 文字列をdouble型に変換
+    */
+    public static void main(String[] args) {
+        double n = Double.parseDouble(args[1]);
+        System.out.println(ceil(n));
+    }
 }
