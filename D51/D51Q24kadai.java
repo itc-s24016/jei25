e10e2237a7af48b0fb4772478185d07ab0624230
itc-s24016 <s24016@std.it-college.ac.jp>
2025-07-04 10:22:38 +0900

    公倍数を求めるプログラム

--- D51/D51Q24kadai.java
+++ D51/D51Q24kadai.java
@@ -4,7 +4,7 @@ public class D51Q24kadai {
     //100未満の3と5の公倍数を出力するコードを作成する
     public static void main(String[] args) {
         for (int x = 1; x < 100; x++){
-            if (x % 3 == 0 || x % 5 == 0){
+            if (x % 3 == 0 && x % 5 == 0){
                 System.out.println(x + ",");
             }
         }
