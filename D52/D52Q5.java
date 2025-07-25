ad443cf49a332d875db92440502e46c3e90b000c
itc-s24016 <s24016@std.it-college.ac.jp>
2025-07-25 10:57:46 +0900

    数値を 5 区切り 1目盛りとして棒グラフで表示する

--- D52/D52Q5.java
+++ D52/D52Q5.java
@@ -7,28 +7,28 @@ public class D52Q5 {
         13 = 5 + 5 + 3
         13 : ***
         */
-
-        String majors = "+----";
-        int LIMIT = 400;
-        if (args.length == 0) return;
+        String majors = "+----";//目盛り
+        int LIMIT = 400;        //整数の上限
+        if (args.length == 0) return; //引数が無い場合は終了
         int max = 0, i = 0;
-        int[] data = new int[args.length];
+        int[] data = new int[args.length];//引数を格納するリスト
+
         for (String s : args) {
-            data[i] = Integer.parseInt(s);
-            if (data[i] <= 0 || data[i] >= LIMIT) return;
+            data[i] = Integer.parseInt(s);//整数に変換
+            if (data[i] <= 0 || data[i] >= LIMIT) return;//0以上400未満の整数しか受け付けない
             if (max < data[i]) {
                 max = data[i];
             }
             i++;
         }
 
-        //目盛り表示
-        int majormax = (max - 1) / 5 + 1;
-        System.out.println("      ");
+        //目盛り表示(可視性の為)
+        int majormax = (max - 1) / 5 + 1;//目盛りの長さ
+        System.out.print("       ");//半角スペース7個分
         for (i = 0; i < majormax; i++) {
-            System.out.print(majors.charAt(i % 5));
+            System.out.print(majors.charAt(i % 5)); // +----+----+ の模様づくり
         }
-        System.out.println("");
+        System.out.println();
 
         //グラフ表示
         for (i = 0; i < data.length; i++) {
@@ -37,7 +37,7 @@ public class D52Q5 {
             for (int j = 0; j < n; j++){
                 System.out.print('*');
             }
-            System.out.println("");
+            System.out.println();
         }
     }
 }
